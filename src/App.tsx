ba32d3f8c02af40444b8f6a1d7fe71581a15e60e
Igor Thierry <igorthierry15@gmail.com>
2020-04-22 15:45:08 -0300

    Signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/SignIn';
+
 const App: React.FC = () => {
-  return <h1>app</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <SignIn />
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: FC = () => {
+  return (
+    <Container>
+      <Content>content</Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
