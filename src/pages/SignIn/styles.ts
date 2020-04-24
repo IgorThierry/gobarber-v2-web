44594bf3d8f63ea7635d7d56f7d7f5db67828a1a
Igor Thierry <igorthierry15@gmail.com>
2020-04-24 20:09:32 -0300

    signIn com components isolados

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,64 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+import singInBg from '../../assets/sign-in-background.png';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
 
-export const Content = styled.div``;
+  display: flex;
+  align-items: stretch;
+`;
 
-export const Background = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 5% 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4dee8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4dee8')};
+      }
+    }
+  }
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${singInBg}) no-repeat center;
+  background-size: cover;
+`;
