7e557c895606c3cc637784f9f3906cb3ade1563c
Igor Thierry <igorthierry15@gmail.com>
2020-09-28 17:05:40 -0300

    forgot password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,15 @@ import Route from './Routes';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/forgot" component={ForgotPassword} />
+
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
