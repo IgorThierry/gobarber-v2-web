55df18ec7b51f7820350aff0681863099c6ab1f1
Igor Thierry <igorthierry15@gmail.com>
2020-10-06 17:42:10 -0300

    next appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,79 @@
 import React from 'react';
+// import Header from '../../components/Header';
 
-// import { Container } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src={
+                user.avatar_url
+                  ? user.avatar_url
+                  : `https://ui-avatars.com/api/?name=${encodeURI(user.name)}`
+              }
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://ui-avatars.com/api/?name=JD" alt="John Doe" />
+              <strong>John Doe</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar>
+          <span />
+        </Calendar>
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
